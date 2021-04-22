409b6a83a23bc93ac8b550f97b719e1b1bb59a64
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2021-04-22 02:29:09 +0200

    Add tests for UsersModule

---
+++ apps/api/src/app/users/users.controller.spec.ts
@@ -0,0 +1,51 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { User } from './user.schema';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+describe('UsersController', () => {
+    let app: TestingModule;
+
+    const userOne = new User('test@example.com');
+    const userTwo = new User('spam@example.com');
+
+    beforeAll(async () => {
+
+        function mockedUserModel(this: any, doc: any) {
+            this.data = doc;
+            this.email = this.data.email;
+            this.save = jest.fn().mockResolvedValue(this.data);
+        }
+        mockedUserModel.find = jest.fn().mockResolvedValue([userOne, userTwo]);
+
+        app = await Test.createTestingModule({
+            controllers: [UsersController],
+            providers: [UsersService, {
+                provide: getModelToken(User.name),
+                useValue: mockedUserModel,
+            }],
+        }).compile();
+    });
+
+    describe('getUsers', () => {
+        it('should return a list of users', async () => {
+            expect.assertions(1);
+            const appController = app.get(UsersController);
+            await expect(appController.getUsers()).resolves.toEqual([
+                userOne, userTwo,
+            ]);
+        });
+    });
+
+    describe('createUser', () => {
+        it('should create and return a user', async () => {
+            expect.assertions(1);
+            const appController = app.get(UsersController);
+            const email = 'new@example.com';
+            const user = await appController.createUser({email});
+            expect(user.email).toEqual(email);
+        });
+    });
+});
---
+++ apps/api/src/app/users/users.service.spec.ts
@@ -0,0 +1,98 @@
+import { ConflictException } from '@nestjs/common';
+import { getModelToken } from '@nestjs/mongoose';
+import { Test } from '@nestjs/testing';
+import { MongoError } from 'mongodb';
+
+import { User } from './user.schema';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+    let service: UsersService;
+
+    const userOne = new User('test@example.com');
+    const userTwo = new User('spam@example.com');
+
+    function mockedUserModel(this: any, doc: any) {
+        this.data = doc;
+        this.email = this.data.email;
+        this.save = jest.fn().mockResolvedValue(new User(this.data.email));
+    }
+    mockedUserModel.find = jest.fn().mockResolvedValue([userOne, userTwo]);
+
+    const createTestUsersService = async (mockedUserModelValue: (this: any, doc: any) => void): Promise<UsersService> => {
+        const app = await Test.createTestingModule({
+            providers: [UsersService, {
+                provide: getModelToken(User.name),
+                useValue: mockedUserModelValue,
+            }],
+        }).compile();
+
+        return app.get(UsersService);
+    };
+
+    beforeAll(async () => {
+        service = await createTestUsersService(mockedUserModel);
+    });
+
+    describe('getUsers', () => {
+        it('should return a list of users', async () => {
+            expect.assertions(1);
+            await expect(service.getUsers()).resolves.toEqual([
+                userOne, userTwo,
+            ]);
+        });
+    });
+
+    describe('createUser', () => {
+        it('should create and return a user', async () => {
+            expect.assertions(1);
+            const email = 'new@example.com';
+            const user = await service.createUser(email);
+            expect(user.email).toEqual(email);
+        });
+
+        it('should throw a ConflictException', async () => {
+            expect.assertions(1);
+
+            const error = new MongoError('Test error!');
+            error.code = 11000;
+
+            service = await createTestUsersService(function(this: any, doc: any) {
+                this.data = doc;
+                this.save = jest.fn().mockRejectedValueOnce(error);
+            });
+
+            const email = 'moo@example.com';
+            await expect(service.createUser(email)).rejects.toEqual(new ConflictException(email));
+        });
+
+        it('should re-throw unknown mongo exceptions', async () => {
+            expect.assertions(1);
+
+            const error = new MongoError('Unknown error!');
+            error.code = 11001;
+
+            service = await createTestUsersService(function(this: any, doc: any) {
+                this.data = doc;
+                this.save = jest.fn().mockRejectedValueOnce(error);
+            });
+
+            const email = 'shiny@example.com';
+            await expect(service.createUser(email)).rejects.toEqual(new MongoError('Unknown error!'));
+        });
+
+        it('should re-throw unknown generic exceptions', async () => {
+            expect.assertions(1);
+
+            const error = new Error('Panic!');
+
+            service = await createTestUsersService(function(this: any, doc: any) {
+                this.data = doc;
+                this.save = jest.fn().mockRejectedValueOnce(error);
+            });
+
+            const email = 'shiny@example.com';
+            await expect(service.createUser(email)).rejects.toEqual(new Error('Panic!'));
+        });
+    });
+});
