19e55baf7f02f75b00a96bc28f864548ce967508
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2021-05-26 00:32:11 +0200

    Add logic to close db connections after testing

--- apps/api/src/tests/tests.utils.ts
+++ apps/api/src/tests/tests.utils.ts
@@ -1,13 +1,14 @@
 import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
-let mongodb: MongoMemoryServer;
+const dbs: MongoMemoryServer[] = [];
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const rootMongooseTestModule = (options: MongooseModuleOptions = {}) => MongooseModule.forRootAsync({
     useFactory: async () => {
-        mongodb = new MongoMemoryServer();
+        const mongodb = new MongoMemoryServer();
         const mongoUri = await mongodb.getUri();
+        dbs.push(mongodb);
         return {
             uri: mongoUri,
             useCreateIndex: true,
@@ -16,8 +17,8 @@ export const rootMongooseTestModule = (options: MongooseModuleOptions = {}) => M
     },
 });
 
-export const closeInMongodbConnection = async (): Promise<void> => {
-    if (mongodb) {
-        await mongodb.stop();
-    }
+export const closeInMongodbConnections = async (): Promise<void> => {
+    await Promise.all(dbs.map(async (db) => {
+        await db.stop();
+    }));
 };
