ee58a0304cc8fd832274b30936ab30ee9913badf
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2021-05-30 01:29:40 +0200

    Update workspace and improve typings

--- apps/api/src/app/guards/auth.guard.ts
+++ apps/api/src/app/guards/auth.guard.ts
@@ -1,9 +1,10 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     public canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest();
+        const request = context.switchToHttp().getRequest<Request>();
         return !!request.session.user;
     }
 }
